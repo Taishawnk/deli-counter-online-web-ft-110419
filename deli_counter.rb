93069f90aa54f260cb27e1961c0354dfb524350b
Taishawn King <taishawnking1@yahoo.com>
2019-11-15 20:02:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,11 +3,11 @@ def line(katz_deli)
   counter = 0
   if katz_deli.size == 0 
   puts "The line is currently empty."
+  
   elsif 
   current_line =[]
   while counter < katz_deli.length 
     current_line << " #{counter +1}. #{katz_deli[counter]}"
-
   counter +=1
     end
     puts "The line is currently:" << current_line.join
